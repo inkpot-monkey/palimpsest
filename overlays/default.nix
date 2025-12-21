88586d0471cdeb362078db205971d9eef828c8ad
thomassdk <thomassdk@pm.me>
2025-12-21 10:49:49 +0100

    feat(nixpkgs): update overlays
    Applied minor tweaks to system-wide overlays for package consistency.

--- overlays/default.nix
+++ overlays/default.nix
@@ -6,13 +6,33 @@
 
   # Custom package modifications
   modifications = final: prev: {
-    tree-sitter = final.unstable.tree-sitter.override {
-      extraGrammars = {
-        tree-sitter-astro = {
-          src = inputs.tree-sitter-astro.outPath;
-        };
+    tree-sitter = prev.tree-sitter.overrideAttrs (old: {
+      passthru = old.passthru // {
+        withPlugins =
+          f:
+          old.passthru.withPlugins (
+            p:
+            f (
+              p
+              // {
+
+                # Define your custom grammars here neatly
+                tree-sitter-astro = final.tree-sitter.buildGrammar {
+                  language = "astro";
+                  version = "master";
+                  src = final.fetchFromGitHub {
+                    owner = "virchau13";
+                    repo = "tree-sitter-astro";
+                    rev = "master";
+                    hash = "sha256-TpXs3jbYn39EHxTdtSfR7wLA1L8v9uyK/ATPp5v4WqE=";
+                  };
+                };
+
+              }
+            )
+          );
       };
-    };
+    });
   };
 
   unstable-packages = final: _prev: {
