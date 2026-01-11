9818d065440628924613397eee46926d3f3645da
thomassdk <thomassdk@pm.me>
2026-01-11 12:56:08 +0100

    chore: autoformat

--- nixos/common/users.nix
+++ nixos/common/users.nix
@@ -1,32 +1,54 @@
-{ inputs, outputs, lib, residents, self, ... }:
+{
+  inputs,
+  outputs,
+  lib,
+  residents,
+  self,
+  ...
+}:
 
 let
   inherit (lib.lists) forEach;
   inherit (lib.attrsets) mergeAttrsList;
-in {
+in
+{
   imports = [ inputs.home-manager.nixosModules.home-manager ];
-
-
 
   home-manager = {
     useUserPackages = true;
     useGlobalPkgs = true;
-    extraSpecialArgs = { inherit inputs outputs residents self; };
+    extraSpecialArgs = {
+      inherit
+        inputs
+        outputs
+        residents
+        self
+        ;
+    };
     backupFileExtension = "backup";
 
-    users = mergeAttrsList (forEach residents
-      (resident: { ${resident.settings.username} = resident.config; }));
+    users = mergeAttrsList (
+      forEach residents (resident: {
+        ${resident.settings.username} = resident.config;
+      })
+    );
   };
 
-  users.users = mergeAttrsList (forEach residents (resident:
-    let inherit (resident.settings) username hashedPassword extraGroups;
-    in {
-      ${username} = {
-        isNormalUser = true;
-        inherit hashedPassword;
-        inherit extraGroups;
-      };
-    }));
+  users.users = mergeAttrsList (
+    forEach residents (
+      resident:
+      let
+        inherit (resident.settings) username hashedPassword extraGroups;
+      in
+      {
+        ${username} = {
+          isNormalUser = true;
+          inherit hashedPassword;
+          inherit extraGroups;
+        };
+      }
+    )
+  );
 
   users.groups = {
     libvirt.members = [ ];
