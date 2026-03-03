cf98ae30a9118a1bc8880d712d53c91034d51f5d
thomassdk <thomassdk@pm.me>
2026-03-03 17:42:46 +0100

    feat(hosts): add sawtoothShark host configuration

---
+++ hosts/sawtoothShark/configuration.nix
@@ -0,0 +1,38 @@
+{
+  self,
+  inputs,
+  ...
+}:
+{
+  imports = [
+    # Hardware
+    ./hardware-configuration.nix
+    ./dell-latitude.nix
+
+    inputs.sops-nix.nixosModules.sops
+    inputs.home-manager.nixosModules.home-manager
+    self.users.general
+    ./../../users/general/plasma.nix
+  ]
+  ++ (with self.nixosProfiles; [
+    # Capabilities
+    base
+    audio
+    gui-base
+    backup
+    direnv
+    fonts
+    gaming
+    impermanence
+    litellm
+    sops
+    monitoring
+  ]);
+
+  sops.age.sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
+
+  networking.hostName = "sawtoothShark";
+  nixpkgs.hostPlatform = "x86_64-linux";
+
+  system.stateVersion = "25.11";
+}
