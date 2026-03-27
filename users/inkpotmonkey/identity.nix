ea33b7ab0e013de024a76f2732ab88b0a3870a6c
Inkpot Monkey <inkpot-monkey@palebluebytes.space>
2026-03-27 10:29:46 +0100

    fix(users): restore original identity data for inkpotmonkey

--- users/inkpotmonkey/identity.nix
+++ users/inkpotmonkey/identity.nix
@@ -4,10 +4,10 @@
   config.custom.users.inkpotmonkey.identity = {
     profile = lib.mkDefault "cli";
     username = "inkpotmonkey";
-    hashedPassword = "$6$u8O13mY7.LIsC6gX$7Sclm0f6.5W6D3A0A6F6H6G6I6J6K6L6M6N6O6P6Q6R6S6T6U6V6W6X6Y6Z606162636465666768696";
-    name = "inkpotmonkey";
-    email = "inkpotmonkey@palebluebytes.xyz";
-    sshKey = "ssh-ed25519 AAAAC3NzaC1lZDI5NTE5AAAAIOm6P5vH79c1H658M8A8B8C8D8E8F8G8H8I8J8K6L8MR2XAihLZ40tIAYoq thomas@palebluebytes.xyz (stargazer)"; # Verified key
+    hashedPassword = "<SCRUBBED_PASSWORD>";
+    name = "thomassdk";
+    email = "<SCRUBBED_EMAIL>";
+    sshKey = "<SCRUBBED_SSH_KEY>";
     extraGroups = [
       "podman"
       "docker"
