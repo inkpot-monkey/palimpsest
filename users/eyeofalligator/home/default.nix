00870b22f0c42fbe395d81922e20117b44712bd1
Inkpot Monkey <inkpot-monkey@palebluebytes.space>
2026-04-24 21:38:27 +0200

    feat(users/eyeofalligator): configure home-manager and user apps

---
+++ users/eyeofalligator/home/default.nix
@@ -0,0 +1,29 @@
+{
+  pkgs,
+  inputs,
+  ...
+}:
+{
+  imports = [
+    inputs.sops-nix.homeManagerModule
+  ];
+
+  sops.age.sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
+  sops.defaultSopsFile = inputs.secrets + "/shared.yaml";
+
+  home.packages = with pkgs; [
+    kdePackages.kate
+    google-chrome
+    firefox
+    spotify
+    libreoffice-qt
+    mpv
+  ];
+
+  xdg.mimeApps = {
+    enable = true;
+    defaultApplications = {
+      "text/plain" = "kate.desktop";
+    };
+  };
+}
